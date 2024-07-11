93b1217c8f69b804a8493762eeadf913e095f335
Sidhart Singh <isidhartsingh@gmail.com>
2024-07-11 23:12:33 +0530

    Add Navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+import { BiSolidBug } from "react-icons/bi";
+
+const Navbar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href={"/"}>
+        <BiSolidBug size={"30"} />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li>
+            <Link
+              href={link.href}
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
