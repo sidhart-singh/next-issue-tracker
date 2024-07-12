3db15a099e04da167c2836f8df5f95a0eb1b33dc
Sidhart Singh <isidhartsingh@gmail.com>
2024-07-12 12:15:35 +0530

    style the active links in the Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,14 @@
-import Link from "next/link";
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { BiSolidBug } from "react-icons/bi";
+import classNames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -14,11 +20,15 @@ const Navbar = () => {
         <BiSolidBug size={"30"} />
       </Link>
       <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li>
+        {links.map((link, i) => (
+          <li key={i}>
             <Link
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames(
+                { "text-zinc-900": link.href === currentPath },
+                { "text-zinc-500": link.href !== currentPath },
+                { "hover:text-zinc-800 transition-colors": true }
+              )}
             >
               {link.label}
             </Link>
