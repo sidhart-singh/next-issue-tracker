7d0682e3407afeab602cb7a208c7e97c52a0f4c0
Sidhart <isidhartsingh@gmail.com>
2024-07-13 20:36:55 +0530

    Add loading skeletons

---
+++ app/issues/IssuesActions.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "@radix-ui/themes";
+
+const IssuesActions = () => {
+  return (
+    <div className="mb-5">
+      <Button>
+        <Link href={"/issues/new"}>New Issue</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default IssuesActions;
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import delay from "delay";
-import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import IssuesActions from "./IssuesActions";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
+const LoadingIssuesPage = () => {
+  const issues = [1, 2, 3, 4, 5];
 
   return (
     <div>
@@ -26,18 +24,18 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                {issue.title}
-                <div className="inline md:hidden mx-2">
-                  <IssueStatusBadge status={issue.status} />
+                <Skeleton />
+                <div className="block md:hidden">
+                  <Skeleton />
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -47,4 +45,4 @@ const IssuesPage = async () => {
   );
 };
 
-export default IssuesPage;
+export default LoadingIssuesPage;
