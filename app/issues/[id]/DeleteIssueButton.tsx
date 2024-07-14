a3c61b1a0c1038284bbc1ef66b81e1506f3cb6f6
Sidhart <isidhartsingh@gmail.com>
2024-07-14 22:13:05 +0530

    Show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { MdOutlineDeleteForever } from "react-icons/md";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <MdOutlineDeleteForever size={20} />
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <MdOutlineDeleteForever size={20} />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure, you want to delete the issue?
+        </AlertDialog.Description>
+        <Flex mt={"4"} gap={"3"}>
+          <AlertDialog.Cancel>
+            <Button variant={"soft"} color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color={"red"}>Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
